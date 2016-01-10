3fd9e5fdcec2818bf9c1548bc4334423421d73bc
Nsgj <475904560@qq.com>
2016-01-10 23:38:42 +0800

    第三次讨论课

---
+++ disc03（tree）.py
@@ -0,0 +1,48 @@
+__author__ = 'computer'
+def tree(value,branches = []):
+    for branch in branches:
+        assert is_tree(branch),'branches must be trees'
+    return [value]+list(branches)
+
+def root(tree):
+    return tree[0]
+
+def branches(tree):
+    return tree[1:]
+
+def is_leaf(tree):
+    return not branches(tree)
+
+def is_tree(tree):
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+t = tree(1,[tree(3,[tree(4),tree(5),tree(6)]),tree(2)])
+print(t)
+def square_tree(tree):
+    root3 = []
+    for i in tree:
+        root1 = []
+        root2 = []
+        if not is_tree(i):
+            root1 = [i * i]
+        else:
+            root2 = [square_tree(i)]
+        root3 += root1 + root2
+    return root3
+
+t2 = square_tree(t)
+
+print(t2)
+
+def height(tree):
+    """return the height of a tree"""
+    n = 0
+    for i in tree:
+        if is_tree(i):
+            n += 1
+            height(i)
